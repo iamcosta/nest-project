c3547c07a5fe0c7f9dbdfea0d01a1e4cdfb262e7
IAM BARROSO DA COSTA <iam.barroso@sonda.com>
2024-07-05 11:05:50 -0400

    relational_data

--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,7 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Article } from '@prisma/client';
+import { Exclude } from 'class-transformer';
+import { UserEntity } from 'src/users/entities/user.entity';
 
 export class ArticleEntity implements Article {
+  constructor({ author, ...data }: Partial<ArticleEntity>) {
+    Object.assign(this, data);
+    if (author) this.author = new UserEntity(author);
+  }
+
   @ApiProperty()
   id: number;
 
@@ -22,4 +29,10 @@ export class ArticleEntity implements Article {
 
   @ApiProperty()
   updatedAt: Date;
+
+  @Exclude()
+  authorId: number;
+
+  @ApiProperty({ required: false, type: UserEntity })
+  author?: UserEntity;
 }
